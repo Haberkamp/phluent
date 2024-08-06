1e5357c4cb197662e014e66a762f644fefccf0f3
Nils Haberkamp <haberkamp.n@gmail.com>
2024-08-06 23:18:47 +0200

    refactor: combine float and int tests classes

--- tests/NumberAssertionTest.php
+++ tests/NumberAssertionTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 use function Phluent\Expect;
 
-class IntegerAssertionTest extends TestCase
+class NumberAssertionTest extends TestCase
 {
     #[Test]
     public function test_passes_when_expecting_an_integer_and_getting_one(): void
@@ -276,6 +276,64 @@ class IntegerAssertionTest extends TestCase
 
         // ACT
         Expect($value)->not()->toBeLessThan(3);
+    }
+
+    #[Test]
+    public function passes_when_expecting_float_and_getting_one(): void
+    {
+        // ARRANGE
+        $value = 1.87;
+
+        // ACT & ASSERT
+        Expect($value)->toBeAFloat();
+    }
+
+    #[Test]
+    #[DataProvider('provideNonFloatValues')]
+    public function fails_when_expecting_a_float_and_not_getting_one(mixed $value): void
+    {
+        // ASSERT
+        $this->expectException(AssertionFailedError::class);
+
+        // ACT
+        Expect($value)->toBeAFloat();
+    }
+
+    #[Test]
+    #[DataProvider('provideNonFloatValues')]
+    public function passes_when_not_expecting_a_float_value_and_not_getting_one(mixed $value): void
+    {
+        // ACT & ASSERT
+        Expect($value)->not()->toBeAFloat();
+    }
+
+    #[Test]
+    public function fails_when_not_expecting_a_float_but_getting_one(): void
+    {
+        // ARRANGE
+        $value = 1.87;
+
+        // ASSERT
+        $this->expectException(AssertionFailedError::class);
+
+        // ACT
+        Expect($value)->not()->toBeAFloat();
+    }
+
+    public static function provideNonFloatValues(): array
+    {
+        return [
+            [null],
+            [true],
+            [false],
+            [''],
+            ['1.87'],
+            ['1'],
+            [1],
+            [0],
+            [[]],
+            [new \stdClass()],
+        ];
     }
 
     public static function provideNonIntegerValues(): array
