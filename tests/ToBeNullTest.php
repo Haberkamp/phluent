ff52e23a985e01194bdee8d159cd8ccc91d6acfc
Nils Haberkamp <haberkamp.n@gmail.com>
2024-08-11 20:26:57 +0200

    fix: improve assertion message for toBeNull assertion

--- tests/ToBeNullTest.php
+++ tests/ToBeNullTest.php
@@ -3,8 +3,8 @@
 namespace Phluent\Tests;
 
 use PHPUnit\Framework\AssertionFailedError;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
 
 use function Phluent\Expect;
@@ -22,18 +22,33 @@ class ToBeNullTest extends TestCase
     }
 
     #[Test]
-    #[DataProvider('provideNonNullValues')]
-    public function fails_when_expecting_null_and_not_getting_null(mixed $value): void
+    #[TestWith([false, 'Expected value to be null, got false.'])]
+    #[TestWith([true, 'Expected value to be null, got true.'])]
+    #[TestWith(['', 'Expected value to be null, got \'\''])]
+    #[TestWith(['some-string', 'Expected value to be null, got \'some-string\''])]
+    #[TestWith([0, 'Expected value to be null, got 0.'])]
+    #[TestWith([1, 'Expected value to be null, got 1.'])]
+    #[TestWith([[], 'Expected value to be null, got Array &0 [].'])]
+    #[TestWith([new \stdClass(), 'Expected value to be null, got stdClass Object #2983 ()'])]
+    public function fails_when_expecting_null_and_not_getting_null(mixed $value, string $message): void
     {
         // ASSERT
         $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessage($message);
 
         // ACT
         Expect($value)->toBeNull();
     }
 
     #[Test]
-    #[DataProvider('provideNonNullValues')]
+    #[TestWith([false])]
+    #[TestWith([true])]
+    #[TestWith([''])]
+    #[TestWith(['some-string'])]
+    #[TestWith([0])]
+    #[TestWith([1])]
+    #[TestWith([[]])]
+    #[TestWith([new \stdClass()])]
     public function passes_when_not_expecting_null_and_not_getting_null(mixed $value): void
     {
         // ACT & ASSERT
@@ -48,21 +63,9 @@ class ToBeNullTest extends TestCase
 
         // ASSERT
         $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessage('Expected value not to be null, got null.');
 
         // ACT
         Expect($value)->not()->toBeNull();
-    }
-
-    public static function provideNonNullValues(): array
-    {
-        return [
-            [false],
-            [true],
-            [''],
-            ['some-string'],
-            [0],
-            [1],
-            [[]],
-        ];
     }
 }
