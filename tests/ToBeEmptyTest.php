5788d9d937bfe0de0a49613c2157ec1fbea5f50a
Nils Haberkamp <haberkamp.n@gmail.com>
2024-08-12 11:42:54 +0200

    refactor: move tests for toBeEmpty assertion into its own test class

---
+++ tests/ToBeEmptyTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Phluent\Tests;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use function Phluent\Expect;
+
+class ToBeEmptyTest extends TestCase
+{
+    #[Test]
+    public function passes_when_expecting_an_empty_string_and_getting_an_empty_string(): void
+    {
+        // ARRANGE
+        $value = '';
+
+        // ACT & ASSERT
+        Expect($value)->toBeEmpty();
+    }
+
+    #[Test]
+    #[DataProvider('provideNonEmptyStrings')]
+    public function fails_when_expecting_an_empty_string_but_getting_a_non_empty_string(string $value): void
+    {
+        // ASSERT
+        $this->expectException(AssertionFailedError::class);
+
+        // ACT
+        Expect($value)->toBeEmpty();
+    }
+
+    #[Test]
+    #[DataProvider('provideNonEmptyStrings')]
+    public function passes_when_not_expecting_an_empty_string_and_not_getting_an_empty_string(string $value): void
+    {
+        // ACT & ASSERT
+        Expect($value)->not()->toBeEmpty();
+    }
+
+    #[Test]
+    public function fails_when_not_expecting_an_empty_string_but_getting_an_empty_string(): void
+    {
+        // ARRANGE
+        $value = '';
+
+        // ASSERT
+        $this->expectException(AssertionFailedError::class);
+
+        // ACT
+        Expect($value)->not()->toBeEmpty();
+    }
+
+    public static function provideNonEmptyStrings(): array
+    {
+        return [
+            [' '],
+            ["Hello, world!"],
+            ["\n"],
+            ["\x00"],
+            ["\t"],
+            ["\r"],
+        ];
+    }
+}
