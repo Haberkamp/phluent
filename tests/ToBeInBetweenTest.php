1e6361357ebaad6d81be9a6aecc019434f54de1c
Nils Haberkamp <haberkamp.n@gmail.com>
2024-08-12 16:49:02 +0200

    fix: improve assertion message for toBeInBetween assertion

--- tests/ToBeInBetweenTest.php
+++ tests/ToBeInBetweenTest.php
@@ -23,16 +23,17 @@ class ToBeInBetweenTest extends TestCase
     }
 
     #[Test]
-    #[TestWith([-2, -1, 5])]
-    #[TestWith([-2.2, -1, 5])]
-    #[TestWith([6, -1, 5])]
-    #[TestWith([6.6, -1, 5])]
-    #[TestWith([-2, -1, 5.5])]
-    #[TestWith([-2, -1.1, 5.5])]
-    public function fails_when_expecting_value_to_be_between_two_numbers_and_it_is_not(int|float $value, int|float $start, int|float $end): void
+    #[TestWith([-2, -1, 5, 'Expected -2 to be in between -1 and 5, but it is not.'])]
+    #[TestWith([-2.2, -1, 5, 'Expected -2.2 to be in between -1 and 5, but it is not.'])]
+    #[TestWith([6, -1, 5, 'Expected 6 to be in between -1 and 5, but it is not.'])]
+    #[TestWith([6.6, -1, 5, 'Expected 6.6 to be in between -1 and 5, but it is not.'])]
+    #[TestWith([-2, -1, 5.5, 'Expected -2 to be in between -1 and 5.5, but it is not.'])]
+    #[TestWith([-2, -1.1, 5.5, 'Expected -2 to be in between -1.1 and 5.5, but it is not.'])]
+    public function fails_when_expecting_value_to_be_between_two_numbers_and_it_is_not(int|float $value, int|float $start, int|float $end, string $message): void
     {
         // ASSERT
         $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessage($message);
 
         // ACT
         Expect($value)->toBeInBetween($start, $end);
@@ -52,14 +53,15 @@ class ToBeInBetweenTest extends TestCase
     }
 
     #[Test]
-    #[TestWith([5, 4, 6])]
-    #[TestWith([4.4, 4, 6])]
-    #[TestWith([5, 4.5, 6])]
-    #[TestWith([5, 4, 6.5])]
-    public function fails_when_expecting_value_not_to_be_in_between_to_numbers_but_it_is(int|float $value, int|float $start, int|float $end): void
+    #[TestWith([5, 4, 6, 'Expected 5 not to be in between 4 and 6, but it is.'])]
+    #[TestWith([4.4, 4, 6, 'Expected 4.4 not to be in between 4 and 6, but it is.'])]
+    #[TestWith([5, 4.5, 6, 'Expected 5 not to be in between 4.5 and 6, but it is.'])]
+    #[TestWith([5, 4, 6.5, 'Expected 5 not to be in between 4 and 6.5, but it is.'])]
+    public function fails_when_expecting_value_not_to_be_in_between_to_numbers_but_it_is(int|float $value, int|float $start, int|float $end, string $message): void
     {
         // ASSERT
         $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessage($message);
 
         // ACT
         Expect($value)->not()->toBeInBetween($start, $end);
