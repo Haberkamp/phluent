db605b1b7c883cf73c26e59c5a9a00bdf9b5a6d7
Nils Haberkamp <haberkamp.n@gmail.com>
2024-08-12 16:43:19 +0200

    fix: improve assertion message forBePositive assertion

--- tests/ToBePositiveTest.php
+++ tests/ToBePositiveTest.php
@@ -21,13 +21,14 @@ class ToBePositiveTest extends TestCase
     }
 
     #[Test]
-    #[TestWith([0])]
-    #[TestWith([-1])]
-    #[TestWith([-1.1])]
-    public function fails_when_expecting_value_to_be_positive_but_it_is_not(mixed $value): void
+    #[TestWith([0, 'Expected value to be positive, got 0'])]
+    #[TestWith([-1, 'Expected value to be positive, got -1'])]
+    #[TestWith([-1.1, 'Expected value to be positive, got -1.1'])]
+    public function fails_when_expecting_value_to_be_positive_but_it_is_not(mixed $value, string $message): void
     {
         // ASSERT
         $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessage($message);
 
         // ACT
         Expect($value)->toBePositive();
@@ -44,12 +45,13 @@ class ToBePositiveTest extends TestCase
     }
 
     #[Test]
-    #[TestWith([1])]
-    #[TestWith([1.1])]
-    public function fails_when_expecting_value_not_to_be_positive_but_it_is(int|float $value): void
+    #[TestWith([1, 'Expected value not to be positive, got 1'])]
+    #[TestWith([1.1, 'Expected value not to be positive, got 1.1'])]
+    public function fails_when_expecting_value_not_to_be_positive_but_it_is(int|float $value, string $message): void
     {
         // ASSERT
         $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessage($message);
 
         // ACT
         Expect($value)->not()->toBePositive();
